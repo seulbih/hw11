d7fde15e2730e77f17c424d2ddaaa05fe3759002
seulbih <tmfq1123@naver.com>
2021-05-24 02:16:00 +0900

    nondigraph로수정, edge추가시 오름차순 정렬

--- graph_search.c
+++ graph_search.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 
 #define MAX_VERTEX 10
-
+#define FALSE 0
+#define TRUE 1
 
 typedef struct graphNode { //인접리스트의 노드 구조체
 	int vertex; //정점
@@ -12,15 +13,28 @@ typedef struct graphNode { //인접리스트의 노드 구조체
 typedef struct Graph { //그래프를 인접리스트로 표현하기 위한 구조체
 	int n; //노드의 갯수
 	graphNode* list[MAX_VERTEX]; //그래프용 인접리스트 생성
+	int visited[MAX_VERTEX]; //flag
 }Graph;
 
-short int visited[MAX_VERTEX];
+typedef struct stack{
+	int data;
+	struct stack *link;
+}stack;
+
+stack* top;
+
+Graph* initialize(Graph* g);
+int freeGraph(Graph* g);
+int insertVertex(Graph* g);
+int insertEdge(Graph* g, int u, int v);
+int DFS(Graph* g, int v);
+int BFS(Graph* g, int v);
+int isEmpty();
+void push(int n);
+int pop();
 
 
-Graph* initialize(Graph *g);
-int freeGraph(Graph *g);
-int insertVertex(Graph *g);
-int insertEdge(Graph *g, int u, int v);
+
 
 int main()
 {
@@ -56,17 +70,23 @@ int main()
 			printf("u = ");
 			fflush(stdout);
 			scanf("%d", &u);
-			printf("u = ");
+			printf("v = ");
 			fflush(stdout);
 			scanf("%d", &v);
 			insertEdge(graph, u, v);
 			break;
 
 		case 'd': case 'D':
-
+			printf("Your Key = ");
+			fflush(stdout);
+			scanf("%d", &u);
+			DFS(graph, u);
 			break;
 
 		case 'b': case 'B':
+			printf("Your Key = ");
+			fflush(stdout);
+			scanf("%d", &u);
 
 			break;
 
@@ -99,6 +119,7 @@ Graph* initialize(Graph* g){
 	temp->n=0; //정점의 갯수=0으로 설정
 	for(v=0; v<MAX_VERTEX; v++){ //그래프리스트 초기화
 		temp->list[v]=NULL;
+		temp->visited[v]=FALSE;
 	}
 	return temp;
 }
@@ -120,6 +141,71 @@ int freeGraph(Graph* g){
 	return 0;
 }
 
+int isEmpty(){ //스택이 비었는지 확인
+	if (top==NULL){
+		return 1;
+	}
+	else
+		return 0;
+}
+
+void push(int n){
+	stack* temp=(stack *)malloc(sizeof(stack));
+	temp->data=n;
+	temp->link=top;
+	top=temp;
+}
+
+int pop(){
+	int item;
+	stack* temp=top;
+
+	if(isEmpty()){
+		printf("\n Stack is empty!!!!\n");
+		return 0;
+	}
+	else{
+		item=temp->data;
+		top=temp->link;
+		free(temp);
+		return item;
+	}
+}
+
+int DFS(Graph* g, int v){
+	graphNode* t;
+	top=NULL;
+	push(v);
+	g->visited[v]=TRUE;
+	printf("[ %d ]", v);
+
+	while(!isEmpty()){
+		v=pop();
+		t=g->list[v];
+
+		while(t){
+			if(!g->visited[t->vertex]){
+				if(isEmpty()){
+					push(v);
+				}
+				push(t->vertex);
+				g->visited[t->vertex]=TRUE;
+				printf("[ %d ]", t->vertex);
+				v=t->vertex;
+				t=g->list[v];
+			}
+			else
+				t=t->link;
+		}
+	}
+
+	return 0;
+}
+int BFS(Graph* g, int v){
+
+	return 0;
+}
+
 int insertVertex(Graph* g){
 
 	if((g->n)+1>MAX_VERTEX){ //그래프 정점 갯수가 10개를 넘을 시
@@ -132,15 +218,73 @@ int insertVertex(Graph* g){
 }
 
 int insertEdge(Graph* g, int u, int v){
-	graphNode* node;
+	graphNode* unode;
+	graphNode* vnode;
+	graphNode* ptr;
+	graphNode* prev;
+
 	if((g->n)<=u || (g->n)<=v){ //그래프에 없는 정점인 경우
 		printf("vertex is not in this graph!!\n");
 		return 0;
 	}
-	node = (graphNode *)malloc(sizeof(graphNode)); //정점 생성
-	node->vertex=v;
-	node->link=g->list[u];
-	g->list[u]=node;
+	ptr=g->list[u];
+	while(ptr){//edge가 이미 존재하는지 확인
+		if(ptr->vertex==v){
+			printf("This edge is already exists!!\n");
+			return 0;
+		}
+		ptr=ptr->link;
+	}
+
+	ptr=g->list[u];
+	prev=NULL;
+	unode = (graphNode *)malloc(sizeof(graphNode)); //정점 생성
+	unode->vertex=v;
+	if(!ptr||ptr->vertex>v){
+			unode->link=g->list[u];
+			g->list[u]=unode;
+		}
+	else{
+		while(ptr){
+		if(ptr->vertex<v){
+			prev=ptr;
+			ptr=ptr->link;
+			}
+		else{
+			break;
+		}
+		}
+		unode->link=prev->link;
+		prev->link=unode;
+	}
+
+
+	/*vnode = (graphNode *)malloc(sizeof(graphNode)); //정점 생성
+	vnode->vertex=u; //digraph가 아니므로 (u,v),(v,u)함께 생성
+	vnode->link=g->list[v];
+	g->list[v]=vnode;*/
+
+	ptr=g->list[v];
+	prev=NULL;
+	vnode = (graphNode *)malloc(sizeof(graphNode)); //정점 생성
+	vnode->vertex=u; //digraph가 아니므로 (u,v),(v,u)함께 생성
+	if(!ptr||ptr->vertex>u){
+			vnode->link=g->list[v];
+			g->list[v]=vnode;
+		}
+	else{
+		while(ptr){
+		if(ptr->vertex<u){
+			prev=ptr;
+			ptr=ptr->link;
+			}
+		else{
+				break;
+				}
+		}
+		vnode->link=prev->link;
+		prev->link=vnode;
+	}
 
 	return 0;
 }
